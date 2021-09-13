9708eee6753ef166a1caf2d46cf2d64721635b1a
Paulo lacerda <xcloudgameemail@gmail.com>
2021-09-13 15:14:19 -0300

    Resolve até o desafio 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+      <Link to="/"><p>Home</p></Link>
+      <Link to="/users"><p>Users</p></Link>
+      <Link to="/about"><p>About</p></Link>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/users" component={Users} />
+      <Route path="/about" component={About} />
+      </BrowserRouter>
     );
   }
 }
